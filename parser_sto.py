5234a8a0c60e91f3423d0e8264e7809d249b27b8
Ivan <vanya909x75@gmail.com>
2021-11-22 20:36:27 +0700

    feat: Добавлена возможность объединять параграфы на одной странице
    Теперь в input data можно размещать параграфы между флагами '\*', чтобы 
    заставить их располагаться на одной странице (функция "не отрывать от 
    следующего" в docx)

--- parser_sto.py
+++ parser_sto.py
@@ -19,33 +19,41 @@ class DocxWriter:
         paragraph_format.first_line_indent = Cm(1.25)
         paragraph_format.space_after = 0
 
+        self.keep_with_next = False
+
 
     def write_to_doc(self, text):
+        if text == '\*':
+            self.keep_with_next = not self.keep_with_next
+            return
+
         if text.startswith('\\!'):
-            self.add_bold_text_paragraph(text[2:])
-        elif text.startswith('&') and text.endswith('&'):
-            self.add_image(text[1:-1])
+            self.add_bold_text_paragraph(text[2:], self.keep_with_next)
+        elif text.strip().startswith('&') and text.strip().endswith('&'):
+            self.add_image(text[1:-1], self.keep_with_next)
         else:
-            self.text_paragraph().add_run(text)
+            self.text_paragraph(self.keep_with_next).add_run(text)
         #elif text.startswith('')
 
 
-    def text_paragraph(self):
+    def text_paragraph(self, keep_with_next=False):
         par = self.doc.add_paragraph()
+        par.paragraph_format.keep_with_next = keep_with_next
         par.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
 
         return par
 
 
-    def add_bold_text_paragraph(self, text):
-        par = self.text_paragraph()
+    def add_bold_text_paragraph(self, text, keep_with_next=False):
+        par = self.text_paragraph(keep_with_next)
         par.add_run(text).bold = True
 
 
-    def add_image(self, text=''):
+    def add_image(self, text='', keep_with_next=False):
         par = self.doc.add_paragraph()
         par.alignment = WD_ALIGN_PARAGRAPH.CENTER
         par.paragraph_format.first_line_indent = 0
+        par.paragraph_format.keep_with_next = keep_with_next
 
         run = par.add_run()
         self.picture_count += 1
